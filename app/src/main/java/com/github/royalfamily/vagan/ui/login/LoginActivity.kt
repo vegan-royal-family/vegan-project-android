9d426591e76bf496b506ee91ef5a20873e1a6ff2
jisulim <chltmdgus06@naver.com>
2022-08-14 22:50:53 +0900

    로그인 버튼 id 잘못 입력된 버그 수정

--- app/src/main/java/com/github/royalfamily/vagan/ui/login/LoginActivity.kt
+++ app/src/main/java/com/github/royalfamily/vagan/ui/login/LoginActivity.kt
@@ -28,7 +28,7 @@ class LoginActivity : BaseActivity() {
 
         // Databinding을 해주었기 때문에, 위에서 선언한 binding 키워드로 xml의 view에 findViewById 없이 접근할 수 있어요.
         // xml에선 snake 표기법을 사용하는데, binding에서 id를 camel 표기법으로 변환하기때문에 btnRequest로 접근합니다.
-        binding.btnRequest.setOnClickListener {
+        binding.btnKakao.setOnClickListener {
             loginWithKakaoTalk()
         }
     }
@@ -55,4 +55,5 @@ class LoginActivity : BaseActivity() {
                 }
             }
     }
+    
 }
